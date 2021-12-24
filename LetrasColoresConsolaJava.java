44dc644b70a97b3f7d09655fea9a61c272674754
antonioHigueron <acobosscabello@gmail.com>
2021-12-24 11:01:15 +0100

    clase para escribir las letras en consola de comandos con distintos colores y fondo

---
+++ LetrasColoresConsolaJava.java
@@ -0,0 +1,14 @@
+/**
+ * LetrasColoresConsolaJava
+ */
+public class LetrasColoresConsolaJava {
+
+    public static void main(String[] args) {
+        System.out.println("hola");
+        System.out.println("\u001B[32m texto \u001B[0m");// letras verdes
+        System.out.println("\u001B[42m texto \u001B[0m");// fondo verde
+        System.out.println("\u001B[31m texto \u001B[0m");// letras rojas
+        System.out.println("\u001B[33m texto \u001B[0m");// letras amarillas 
+        System.out.println("tal");
+    }
+}
