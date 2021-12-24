18c1bcaa547e4b8b085441ce4af1682a3feea49c
antonioHigueron <acobosscabello@gmail.com>
2021-12-24 11:19:27 +0100

    add new colour for letter

--- LetrasColoresConsolaJava.java
+++ LetrasColoresConsolaJava.java
@@ -3,6 +3,27 @@
  */
 public class LetrasColoresConsolaJava {
 
+   //Reset
+   public static final String ANSI_RESET = "\u001B[0m";
+   //Colores de letra
+   public static final String ANSI_BLACK = "\u001B[30m";
+   public static final String ANSI_RED = "\u001B[31m";
+   public static final String ANSI_GREEN = "\u001B[32m";
+   public static final String ANSI_YELLOW = "\u001B[33m";
+   public static final String ANSI_BLUE = "\u001B[34m";
+   public static final String ANSI_PURPLE = "\u001B[35m";
+   public static final String ANSI_CYAN = "\u001B[36m";
+   public static final String ANSI_WHITE = "\u001B[37m";
+   //Colores de fondo
+   public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+   public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+   public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+   public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+   public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+   public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+   public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+   public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
     public static void main(String[] args) {
         System.out.println("hola");
         System.out.println("\u001B[32m texto \u001B[0m");// letras verdes
@@ -10,5 +31,11 @@ public class LetrasColoresConsolaJava {
         System.out.println("\u001B[31m texto \u001B[0m");// letras rojas
         System.out.println("\u001B[33m texto \u001B[0m");// letras amarillas 
         System.out.println("tal");
+        System.out.println("\n\nColor normal!!\n\n");
+        System.out.println(ANSI_RED + "Texto de color rojo" + ANSI_RESET);
+        System.out.println(ANSI_GREEN_BACKGROUND + ANSI_BLUE + "Texto de color azul sobre fondo verde");
+        System.out.println("Como no lo he resetado sigue igual.");
+        System.out.println("¡Reseteo ahora!" + ANSI_RESET);
+        System.out.println("FIN");
     }
 }
